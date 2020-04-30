52050a40009026be3b1d23d3aa8613af70bfd4a6
thecodeninja <64408638+thecodeninja-ir@users.noreply.github.com>
2020-04-30 12:56:21 +0430

    update code

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-function openNav(type){
-    if(type == 'pushcontnet'){
-        document.getElementById('sidenavabar').style.width = "250px";
-        document.getElementById('content').style.marginLeft = '250px';
-    }else{
-        document.getElementById('sidenavabar').style.width = "250px";
-    }
+function openNav(){
+    document.getElementById('sidenavbar').style.width = "250px";
 }
 
-function closeNave(){
-    document.getElementById('sidenavabar').style.width = "0px";
-    document.getElementById('content').style.marginLeft = '0px';
+function pushContent(){
+    document.getElementById('sidenavbar').style.width = "250px";
+    document.getElementById('content').style.marginLeft = "250px";
+}
+
+function closeNav(){
+    document.getElementById('sidenavbar').style.width = "0px";
+    document.getElementById('content').style.marginLeft = "0px";
 }
